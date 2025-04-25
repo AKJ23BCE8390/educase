bed0b1e3b6696c85fb91f0a6493bfe2815263426
Ankit <ankitkumarjhajharia001@gmail.com>
2025-04-25 12:22:16 +0530

    updated config file

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,6 @@ import autoprefixer from 'autoprefixer'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss(),
-        autoprefixer()
-      ]
-    }
-  },
+  
 })
 
